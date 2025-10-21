ceb1fc4e762f1b2a26dbf50eea9c959d6f1ad54e
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-10-21 23:31:26 +0500

    add cors

--- Conf/settings.py
+++ Conf/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
 
     'app_user',
     'app_common'
@@ -37,13 +38,19 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'Conf.urls'
 
