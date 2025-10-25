126b15f7525562935a98c5af57d6994e7bf021bd
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-10-25 15:34:32 +0500

    update rest settings

--- Conf/settings.py
+++ Conf/settings.py
@@ -186,11 +186,17 @@ SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,  # Har sahifada 10 ta post
+    'PAGE_SIZE': 10,
+
     'DEFAULT_FILTER_BACKENDS': [
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+
 }
 
 
